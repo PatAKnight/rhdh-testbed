9c92779d53481313d22d3a03cd5c2f9dcd0cb601
Patrick Knight <pknight@redhat.com>
2026-02-16 10:30:32 -0500

    Create release-1.8 branch
    Signed-off-by: Patrick Knight <pknight@redhat.com>

--- env_variables.sh
+++ env_variables.sh
@@ -2,16 +2,16 @@
 set -a  # Automatically export all variables
 
 # Use this for the RHDH chart from the Red Hat Developer GitHub repository
-HELM_REPO_NAME=rhdh-chart
-HELM_REPO_URL=https://redhat-developer.github.io/rhdh-chart/
-HELM_CHART_NAME=backstage
-HELM_CHART_VERSION=5.2.0  # community version numbering
+# HELM_REPO_NAME=rhdh-chart
+# HELM_REPO_URL=https://redhat-developer.github.io/rhdh-chart/
+# HELM_CHART_NAME=backstage
+# HELM_CHART_VERSION=5.2.0  # community version numbering
 
 # Use this for the upstream Red Hat Developer Hub chart
-# HELM_REPO_NAME=openshift-helm-charts
-# HELM_REPO_URL=https://charts.openshift.io
-# HELM_CHART_NAME=redhat-developer-hub
-# HELM_CHART_VERSION=1.9.0
+HELM_REPO_NAME=openshift-helm-charts
+HELM_REPO_URL=https://charts.openshift.io
+HELM_CHART_NAME=redhat-developer-hub
+HELM_CHART_VERSION=1.8.0
 OVERALL_RESULT=0
 
 RELEASE_NAME="backstage"
